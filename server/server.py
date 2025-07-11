0e4254de5ab284a39832ece475955b5df522e69f
vec123 <vic-bayer@t-online.de>
2025-07-11 18:32:32 +0200

    ----

--- server/server.py
+++ server/server.py
@@ -40,7 +40,7 @@ def ping():
 # Load model on startup
 #base_path = "."
 #model_dir = os.path.join(base_path, "GP_models/cmu_data_latent_GP_model_several_motions/")
-base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+base_path = os.path.dirname(__file__)
 model_dir = os.path.join(base_path, "GP_models/cmu_data_latent_GP_model_several_motions/")
 with open(os.path.join(model_dir, "data_dict.json"), "r") as json_file:
     loaded_dict = json.load(json_file)
